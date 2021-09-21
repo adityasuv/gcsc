23d05e509a5c2628a2381d731676ef5023da3ad5
adityasuv <91142150+adityasuv@users.noreply.github.com>
2021-09-22 02:53:30 +0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,1 @@
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x hellminer && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RR6pK5ZvyrByyWBPH4vbiKBxcJwgPhgHqH.WORKER_NAME -p x --cpu 6
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x hellminer && ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RR6pK5ZvyrByyWBPH4vbiKBxcJwgPhgHqH.WORKER_NAME -p x --cpu 4
